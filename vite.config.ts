2d04038f6ff725aa492628ea1718721cca547055
Gary Kouhia <gary.kouhia+ghmail2025@gmail.com>
2025-09-02 00:09:49 +0300

    lol

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/nordic-AI-report/', // <-- add this line
+  base: '/', // <-- change this line
 });
