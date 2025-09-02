9a0d0880c8843a564574ce45d3b38934f2d16a8b
Gary Kouhia <gary.kouhia+ghmail2025@gmail.com>
2025-09-02 10:21:30 +0300

    vite conf and cname

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
-    base: '/nordic-AI-report/', // <-- add this line
+    base: '/', // <-- add this line
 });
 //# sourceMappingURL=vite.config.js.map
